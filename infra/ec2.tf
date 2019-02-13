020a7e15b0b18e20f562eec2b74d5b434aabccab
Dawson Mortenson <dawson.mortenson@gmail.com>
2019-02-12 21:54:24 -0800

    update demo terraform

--- infra/ec2.tf
+++ infra/ec2.tf
@@ -3,15 +3,38 @@ resource "aws_key_pair" "my_key" {
   public_key = "${var.pub_key}"
 }
 
-resource "aws_instance" "web" {
-  ami           = "ami-032509850cf9ee54e"
-  instance_type = "t2.micro"
+resource "aws_security_group" "allow_ingress_from_home" {
+  name        = "allow-home"
+  description = "Allow inbound traffic from my home IP"
 
-  tags = {
-    Name = "test"
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["${var.home_ip}"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
+resource "aws_instance" "demo_instance" {
+  ami                    = "ami-032509850cf9ee54e"
+  instance_type          = "t2.micro"
+  key_name               = "${aws_key_pair.my_key.key_name}"
+  vpc_security_group_ids = ["${aws_security_group.allow_ingress_from_home.id}"]
+
+  tags = {
+    Name = "demo-instance"
+  }
+
+  depends_on = ["aws_security_group.allow_ingress_from_home"]
+}
+
 output "instance_ips" {
-  value = ["${aws_instance.web.*.public_ip}"]
+  value = ["${aws_instance.demo_instance.*.public_ip}"]
 }
--- infra/vars.tf
+++ infra/vars.tf
@@ -1,1 +1,5 @@
+# My laptops public key to enable easy ssh access
 variable "pub_key" {}
+
+# My current home IP used to lock down the EC2 access
+variable "home_ip" {}
